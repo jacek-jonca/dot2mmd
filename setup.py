1d0a8b11db8904a9a2cb0db7ccfd11d87be8668b
jacek-jonca <jacek@jonca.org>
2025-11-01 09:36:04 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,18 +4,18 @@ Run `pip install .` to install.
 Run `pip install -e .` to install in editable mode for development.
 """
 
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
     name="dot2mmd",
     version="0.1.0",
-    packages=["dot2mmd"],
-    author="TripleJ",
+    packages=find_packages(include=['dot2mmd', 'dot2mmd.*']),
+    author="TripleJ Jacek",
     author_email="jjonca@gmail.com",
     description="A utility to convert Graphviz DOT files to MermaidJS syntax.",
     long_description="A utility to convert Graphviz DOT files to MermaidJS syntax.",
     long_description_content_type="text/markdown",
-    url="https://github.com/jacek-jonca/dot2mmd", # Replace with your repo
+    url="https://github.com/jacek-jonca/dot2mmd",
     
     install_requires=[
         "pyparsing>=3.0.0",
